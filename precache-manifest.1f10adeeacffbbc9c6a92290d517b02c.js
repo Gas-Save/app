d4d86f9e098c306967aef2a11ecd58c2e52a2e44
Hephyrius <20884880+Hephyrius@users.noreply.github.com>
2021-05-19 17:40:51 +0100

    import site

---
+++ precache-manifest.1f10adeeacffbbc9c6a92290d517b02c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "efe371bfb7b363b22fc4",
+    "url": "/static/css/main.91c078af.chunk.css"
+  },
+  {
+    "revision": "efe371bfb7b363b22fc4",
+    "url": "/static/js/main.efe371bf.chunk.js"
+  },
+  {
+    "revision": "9dc5f719e67402cbb22a",
+    "url": "/static/js/1.9dc5f719.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f63e9312c6ab12b62bcb24db2e16b723",
+    "url": "/static/media/index.f63e9312.mjs"
+  },
+  {
+    "revision": "73b90973b20db96785e4c76d54a67a3d",
+    "url": "/static/media/logo.73b90973.svg"
+  },
+  {
+    "revision": "bce1c25414f9806205b8f2612fa62852",
+    "url": "/static/media/logoGrey.bce1c254.svg"
+  },
+  {
+    "revision": "9e142c76b247cb3e8dca5e363e7f96f0",
+    "url": "/static/media/walletconnect-logo.6bd40fcf.png"
+  },
+  {
+    "revision": "c64d56432276467a7425862320c3025e",
+    "url": "/index.html"
+  }
+];
